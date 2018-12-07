0a0c518737e0f82b47bfb6bc0e7b037910ec10e5
Derrick <derrickhuang2333@gmail.com>
2018-12-07 23:30:14 +1000

    Yes!

---
+++ build/precache-manifest.afb9fa6b5f6f79318c00d4e12395d572.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a4a7900182ccb67793b",
+    "url": "/static/js/main.8a4a7900.chunk.js"
+  },
+  {
+    "revision": "3319ec2a6a4b7ae0d739",
+    "url": "/static/js/1.3319ec2a.chunk.js"
+  },
+  {
+    "revision": "8a4a7900182ccb67793b",
+    "url": "/static/css/main.39870fe1.chunk.css"
+  },
+  {
+    "revision": "3319ec2a6a4b7ae0d739",
+    "url": "/static/css/1.20bf67d6.chunk.css"
+  },
+  {
+    "revision": "803a0c6f16b156ead6e8b8473375f192",
+    "url": "/index.html"
+  }
+];
