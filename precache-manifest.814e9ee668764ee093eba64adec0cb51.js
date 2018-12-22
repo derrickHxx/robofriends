cbfed2c41a7b343910721df4060c6e0b2f57605b
Derrick <derrickhuang2333@gmail.com>
2018-12-22 20:05:23 +1000

    Updates

---
+++ precache-manifest.814e9ee668764ee093eba64adec0cb51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "3342c13954e17f3af7af",
+    "url": "/robofriends/static/js/main.3342c139.chunk.js"
+  },
+  {
+    "revision": "5b21acdacb7d0b8c31b2",
+    "url": "/robofriends/static/js/1.5b21acda.chunk.js"
+  },
+  {
+    "revision": "3342c13954e17f3af7af",
+    "url": "/robofriends/static/css/main.0fd54155.chunk.css"
+  },
+  {
+    "revision": "5b21acdacb7d0b8c31b2",
+    "url": "/robofriends/static/css/1.95f162d6.chunk.css"
+  },
+  {
+    "revision": "e9f70c91eda56175eba4204f7a84f49b",
+    "url": "/robofriends/index.html"
+  }
+];
